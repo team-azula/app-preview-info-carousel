835e5abd0f173eb8fe0bfe15d9ca65c7f1efeb6c
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-07-09 22:09:58 -0700

    bulk insert of up to 100,000 records to postgres: - first implementation:   - using an array of promises and promise.all()   - hard coded single datapoint repeatedly entered - times:   - 1000 entries = 0.584 seconds   - 10000 entries = 4.568 seconds   - 100000 entries = 57.78 seconds

--- database/postgres/pgSeed.js
+++ database/postgres/pgSeed.js
@@ -2,67 +2,42 @@ const faker = require('faker');
 const db = require('./pgIndex.js');
 
 
-// let firstObj = [
-//   {
-//     "preview_data": {
-//       "images": [
-//         'url number 1',
-//         'url number AAAAAAAAAAAAAAAAAA',
-//         'url number asdjkfjasdf3',
-//         'url number 4'
-//       ],
-//       "app_description": "X",
-//       "additional_text": "Y"
-//     }
-//   },
-//   {
-//     "preview_data": {
-//       "images": [
-//         'url number 1',
-//         'url number BBBBBBBBBBBBBBBBBb',
-//         'url number asdjkfjasdf3',
-//         'url number 4'
-//       ],
-//       "app_description": "Xxxx",
-//       "additional_text": "Yxxx"
-//     }
-//   },
-//   {
-//     "preview_data": {
-//       "images": [
-//         'url number 1',
-//         'url number CCCCCCCCCCCCCCCCCCCCc',
-//         'url number asdjkfjasdf3',
-//         'url number 4'
-//       ],
-//       "app_description": "Xxxxxxxxxxxxxxxxxxx",
-//       "additional_text": "Yxxxxxxxxxxxxxxxxxx"
-//     }
-//   },
-//   {
-//     "preview_data": {
-//       "images": [
-//         'url number 1',
-//         'url number DDDDDDDDDDDDDDDDDDdd',
-//         'url number asdjkfjasdf3',
-//         'url number 4'
-//       ],
-//       "app_description": "Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-//       "additional_text": "Yxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-//     }
-//   }
-// ];
+let firstObj = [
+  {
+    "preview_data": {
+      "images": [
+        'url number 1',
+        'url number AAAAAAAAAAAAAAAAAA',
+        'url number asdjkfjasdf3',
+        'url number 4'
+      ],
+      "app_description": "X",
+      "additional_text": "Y"
+    }
+  }
+];
 
 
 
-let count = 1;
+let dataSize = 100000;
+let startTime = null;
+let endTime = null;
+
 const seedPostgresDb = () => {
   return db.initDb()
   .then(() => {
-    return db.addBulkApps(firstObj)
+    startTime = new Date().valueOf();
+    let promiseArray = [];
+    for (let i = 0; i < dataSize; i++) {
+      promiseArray.push(db.addBulkApps(firstObj))
+    }
+    return Promise.all(promiseArray);
   })
   .then((response) => {
-    console.log('response from addSingleApp: ', response);
+    // console.log('response from addSingleApp: ', response);
+    endTime = new Date().valueOf();
+    let totalTime = ((endTime - startTime) / 1000);
+    console.log(`total time to add ${dataSize} to db was ${totalTime}`);
   })
   .catch((err) => {
     console.log('error inside seedPostgresDb: ', err);
