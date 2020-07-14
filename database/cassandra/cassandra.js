b9e39163f1a2149c72440a42efd24db96e6994b9
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-07-14 15:39:30 -0700

    cassandra connected inside container with cassandra-driver

---
+++ database/cassandra/cassandra.js
@@ -0,0 +1,41 @@
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+  contactPoints: ['172.18.0.3'],
+  protocalOptions: { port: '9042' },
+  keyspace: 'cassandra_keyspace_01',
+  localDataCenter: 'datacenter1'
+});
+
+// console.log('client: ', client);
+
+const getStateOfCassandra = async () => {
+  await client.connect();
+  const state = client.getState();
+  console.log('state: ', state);
+  const hosts = state.getConnectedHosts();
+  console.log('hosts: ', hosts);
+};
+
+// getStateOfCassandra();
+
+
+const getOneById = async (id) => {
+  console.log('id: ', id);
+  await client.connect();
+  const getOneQuery = `SELECT id,images,app_description,additional_text FROM app WHERE id = ${id}`;
+  let result = await client.execute(getOneQuery, id);
+  console.log('result.rows from getOneById: ', result.rows);
+  return result;
+};
+
+
+// getOneById('82b3dc30-d37e-4be8-9167-f698cedc088b')
+//   .then((result) => {
+//     console.log('result: ', result);
+//   })
+//   .catch((err) => {
+//     console.log('err: ', err);
+//   })
+
+module.exports = { getStateOfCassandra, getOneById };
